18137d1c00b9f338153ad236ea1fe2eb3fa8207b
bruian <bruianio@gmail.com>
2009-08-14 20:28:30 +0800

    First

---
+++ jkBind/thirdparty/suppressWarnings.h
@@ -0,0 +1,6 @@
+#pragma warning( disable : 4244 )   //conversion from 'xxx' to 'yyy', possible loss of data
+#pragma warning( disable : 4267 )   //conversion from 'size_t' to 'int', possible loss of data
+#pragma warning( disable : 4305 )   //'initializing' : truncation from 'double' to 'float'
+#pragma warning( disable : 4311 )   //pointer truncation from 'SQRefCounted *const ' to 'SQInteger'
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_NON_CONFORMING_SWPRINTFS
